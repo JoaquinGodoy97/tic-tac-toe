264f31c492418cc9ed5584029f5e984f3d305d45
Joaquin Godoy <godoy.joaquin1997@gmail.com>
2021-02-19 14:28:45 -0300

    fixing disabled buttons when game finish

--- Index.js
+++ Index.js
@@ -10,8 +10,9 @@ var boton7 = document.getElementById("boton7");
 var boton8 = document.getElementById("boton8");
 var boton9 = document.getElementById("boton9");
 
+
 function changeButton(boton) {
-    boton.disable = true;
+  boton.disabled = true;
     if (turn1 == true) {
         boton.style="color:blue";
          turn1 = false;
@@ -25,46 +26,36 @@ function changeButton(boton) {
 }
   
   function startTheGame(){
-boton1.disabled = false;
-boton2.disabled = false;
-boton3.disabled = false;
-boton4.disabled = false;
-boton5.disabled = false;
-boton6.disabled = false;
-boton7.disabled = false;
-boton8.disabled = false;
-boton9.disabled = false;
+    reset();
+// boton1.disabled = false;
+// boton2.disabled = false;
+// boton3.disabled = false;
+// boton4.disabled = false;
+// boton5.disabled = false;
+// boton6.disabled = false;
+// boton7.disabled = false;
+// boton8.disabled = false;
+// boton9.disabled = false;
 
-  if (document.getElementById("winner").innerHTML = "Ganaron rojos."){
-  boton1.style.color = "black";
-  boton2.style.color = "black";
-  boton3.style.color = "black";
-  boton4.style.color = "black";
-  boton5.style.color = "black";
-  boton6.style.color = "black";
-  boton7.style.color = "black";
-  boton8.style.color = "black";
-  boton9.style.color = "black";
-  }
+  // if (document.getElementById("winner").innerHTML == "Ganaron azules." || document.getElementById("winner").innerHTML == "Ganaron rojos."){
+  // boton1.style.color = "black";
+  // boton2.style.color = "black";
+  // boton3.style.color = "black";
+  // boton4.style.color = "black";
+  // boton5.style.color = "black";
+  // boton6.style.color = "black";
+  // boton7.style.color = "black";
+  // boton8.style.color = "black";
+  // boton9.style.color = "black";
+  // }
 
-  if (document.getElementById("winner").innerHTML = "Ganaron azules.") {
-  boton1.style.color = "black";
-  boton2.style.color = "black";
-  boton3.style.color = "black";
-  boton4.style.color = "black";
-  boton5.style.color = "black";
-  boton6.style.color = "black";
-  boton7.style.color = "black";
-  boton8.style.color = "black";
-  boton9.style.color = "black";
-  }
 
-  if (document.getElementById("winner").innerHTML = "JUEGA") {
+  if (document.getElementById("winner").innerHTML == "JUEGA") {
     document.getElementById("winner").innerHTML = "Ta Te Ti";
   }
 }
 
-function ganaste() {
+function ganaste(){
   //red
   //horizontal
   if (boton1.style.color == "red" && boton2.style.color == "red" && boton3.style.color == "red"){
@@ -121,5 +112,51 @@ function ganaste() {
    if (boton3.style.color == "blue" && boton5.style.color == "blue" && boton7.style.color == "blue"){
   document.getElementById("winner").innerHTML = "Ganaron azules.";
   }
+  if (document.getElementById("winner").innerHTML == "Ganaron azules." || document.getElementById("winner").innerHTML == "Ganaron rojos."){
+    disableAll();
+  document.getElementById("empezar").innerHTML = "De nuevo?";
+  }
 }
 
+function disableAll(){
+  // let boton_list = [boton1, boton2, boton3, boton4, boton5, boton6, boton7, boton8, boton9];
+  boton1.disabled = true;
+  boton2.disabled = true;
+  boton3.disabled = true;
+  boton4.disabled = true;
+  boton5.disabled = true;
+  boton6.disabled = true;
+  boton7.disabled = true;
+  boton8.disabled = true;
+  boton9.disabled = true;
+}
+
+function reset() {
+  document.getElementById("winner").innerHTML = "JUEGA";
+  document.getElementById("empezar").innerHTML = "Juega";
+
+  boton1.disabled = false;
+  boton2.disabled = false;
+  boton3.disabled = false;
+  boton4.disabled = false;
+  boton5.disabled = false;
+  boton6.disabled = false;
+  boton7.disabled = false;
+  boton8.disabled = false;
+  boton9.disabled = false;
+  colorReset();
+}
+
+function colorReset() {
+  if (document.getElementById("empezar").innerHTML == "Juega"){
+    boton1.style.color = "black";
+    boton2.style.color = "black";
+    boton3.style.color = "black";
+    boton4.style.color = "black";
+    boton5.style.color = "black";
+    boton6.style.color = "black";
+    boton7.style.color = "black";
+    boton8.style.color = "black";
+    boton9.style.color = "black";
+    }
+}
